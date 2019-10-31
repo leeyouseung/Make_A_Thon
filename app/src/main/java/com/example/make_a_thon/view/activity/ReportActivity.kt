c69c78f737208caa0e477b265299c23ab58f6da3
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-10-31 20:42:57 +0900

    [FIXING] report.xml

--- app/src/main/java/com/example/make_a_thon/view/activity/ReportActivity.kt
+++ app/src/main/java/com/example/make_a_thon/view/activity/ReportActivity.kt
@@ -1,13 +1,16 @@
 package com.example.make_a_thon.view.activity
 
 import android.os.Bundle
+
+import androidx.lifecycle.Observer
+
 import com.example.make_a_thon.BR
 import com.example.make_a_thon.R
-import com.example.make_a_thon.base.activity.BaseActivity
+import com.example.make_a_thon.base.activity.BasePictureActivity
 import com.example.make_a_thon.databinding.ActivityReportBinding
 import com.example.make_a_thon.viewmodel.ReportViewModel
 
-class ReportActivity : BaseActivity<ActivityReportBinding, ReportViewModel>() {
+class ReportActivity : BasePictureActivity<ActivityReportBinding, ReportViewModel>() {
 
     override fun getLayoutId(): Int {
         return R.layout.activity_report
@@ -24,6 +27,10 @@ class ReportActivity : BaseActivity<ActivityReportBinding, ReportViewModel>() {
     override fun initObserver() {
         with(viewModel) {
 
+            goToAlbumEvent.observe(this@ReportActivity, Observer {
+                tedPermission()
+                goToAlbum()
+            })
         }
     }
 
--- app/src/main/java/com/example/make_a_thon/viewmodel/ReportViewModel.kt
+++ app/src/main/java/com/example/make_a_thon/viewmodel/ReportViewModel.kt
@@ -3,8 +3,15 @@ package com.example.make_a_thon.viewmodel
 import android.app.Application
 
 import com.example.make_a_thon.base.viewmodel.BaseViewModel
+import com.example.make_a_thon.widget.SingleLiveEvent
 
 class ReportViewModel(application: Application) : BaseViewModel<ReportViewModel>(application) {
+
+    val goToAlbumEvent = SingleLiveEvent<Unit>()
+
+    fun onClickInputImgBtn() {
+        goToAlbumEvent.call()
+    }
 
     override fun onRetrieveDataSuccess(data: ReportViewModel) {}
 
