d219c31815ec2d637100f1cf9b4914aadcee5e4e
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-11-01 16:43:07 +0900

    set

--- app/src/main/java/com/example/make_a_thon/model/report/ReportList.kt
+++ app/src/main/java/com/example/make_a_thon/model/report/ReportList.kt
@@ -1,6 +1,7 @@
 package com.example.make_a_thon.model.report
 
-class ReportList(var time: String,
+class ReportList(var reportId: String,
+                 var time: String,
                  var place: String,
                  var picture: String,
                  var content: String)
--- app/src/main/java/com/example/make_a_thon/network/api/ReportApi.kt
+++ app/src/main/java/com/example/make_a_thon/network/api/ReportApi.kt
@@ -7,8 +7,8 @@ import io.reactivex.Single
 
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import retrofit2.http.GET
 
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
