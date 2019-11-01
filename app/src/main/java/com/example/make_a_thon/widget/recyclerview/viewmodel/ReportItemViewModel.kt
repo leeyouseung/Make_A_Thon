f389be281cedb67fd7b29d1d6b67b13aa592986d
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-11-02 08:47:12 +0900

    Update ReportItemViewModel.kt

--- app/src/main/java/com/example/make_a_thon/widget/recyclerview/viewmodel/ReportItemViewModel.kt
+++ app/src/main/java/com/example/make_a_thon/widget/recyclerview/viewmodel/ReportItemViewModel.kt
@@ -1,12 +1,10 @@
 package com.example.make_a_thon.widget.recyclerview.viewmodel
 
-import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
 
 import com.example.make_a_thon.base.viewmodel.BaseItemViewModel
 import com.example.make_a_thon.model.report.ReportList
 import com.example.make_a_thon.widget.recyclerview.navigator.ReportItemNavigator
-import java.text.SimpleDateFormat
 
 class ReportItemViewModel : BaseItemViewModel<ReportList, ReportItemNavigator>() {
 
@@ -14,12 +12,6 @@ class ReportItemViewModel : BaseItemViewModel<ReportList, ReportItemNavigator>()
     val content = MutableLiveData<String>()
 
     override fun bind(data: ReportList) {
-
-//        @SuppressLint("SimpleDateFormat") val myFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-//
-//        var rescueDate = myFormat.format(data.createdAt).split("T")[0]
-//        var rescueTime = myFormat.format(data.createdAt).split("T")[1].split(".")[0]
-
         time.value = data.createdAt + "  완료"
         content.value = data.content
     }
